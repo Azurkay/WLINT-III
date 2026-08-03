7b135692ab7fe56982412c2b72702849194cc338
Azurkay <baptiste.lassalle38@gmail.com>
2026-08-03 16:51:58 +0200

    my car move forward and the wheelis spinning

--- WhiteLineInterception III/Assets/Scripts/CamaroDrive.cs
+++ WhiteLineInterception III/Assets/Scripts/CamaroDrive.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CamaroDrive : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    #region Serialized Attributes
+
+    [SerializeField] private WheelCollider _FL;
+    [SerializeField] private WheelCollider _FR;
+    [SerializeField] private WheelCollider _RL;
+    [SerializeField] private WheelCollider _RR;
+
+    [SerializeField] private Transform _FLTransform;
+    [SerializeField] private Transform _FRTransform;
+    [SerializeField] private Transform _RLTransform;
+    [SerializeField] private Transform _RRTransform;
+
+    #endregion
+
+    #region Attributes
         
+    #endregion
+
+    private void MoveForward()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            _FL.motorTorque = 100f;
+            RotateWheel(_FL, _FLTransform);
+            _FR.motorTorque = 100f;
+            RotateWheel(_FR, _FRTransform);
+        }
+        else if (Input.GetKeyUp(KeyCode.Z))
+        {
+            _FL.motorTorque = 0;
+            _FR.motorTorque = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RotateWheel(WheelCollider wheelCollider, Transform transform)
     {
-        
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        transform.position = pos;
+        transform.rotation = rot;
     }
+
+
+
+
+
+    #region Mono
+
+    private void Update()
+    {
+        MoveForward();
+    }
+
+    #endregion
 }
