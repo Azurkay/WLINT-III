eb473c732a001facddbf443605eeb59f9e9b4b42
Azurkay <baptiste.lassalle38@gmail.com>
2026-09-15 12:32:58 +0200

    finished the UI Canva remake

--- WhiteLineInterception III/Assets/Scripts/BreakForwardGears.cs
+++ WhiteLineInterception III/Assets/Scripts/BreakForwardGears.cs
@@ -1,19 +1,21 @@
-using Unity.VisualScripting;
+using System;
+using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Scripting.APIUpdating;
+
 
 public class BreakForwardGears : MonoBehaviour
 {
 
     [SerializeField] private CamaroDrive _camaroRef;
     [SerializeField] private float _timeToBreak = 5;
-    [SerializeField] private GameObject _explanationGameObject;
+    [SerializeField] private TextMeshProUGUI _explanationGameObject;
+    [SerializeField] private String _explanationText = "Oh the forward gears juste broked";
     [SerializeField] private float _timeToDesableExplanation;
 
     void Start()
     {
-        Object.Destroy(_explanationGameObject, _timeToDesableExplanation);
+        _explanationGameObject.text = "";
+        Destroy(gameObject, _timeToDesableExplanation);
     }
 
 
@@ -22,7 +24,7 @@ public class BreakForwardGears : MonoBehaviour
         _timeToBreak -= Time.deltaTime;
         if (_timeToBreak <= 0)
         {
-            _explanationGameObject.SetActive(true);
+            _explanationGameObject.text = _explanationText;
             for (int i = 0; i < _camaroRef.GearsRatios.Length; i++)
             {
                 if (_camaroRef.GearsRatios[i] > 0f)
@@ -30,7 +32,6 @@ public class BreakForwardGears : MonoBehaviour
                     _camaroRef.GearsRatios[i] = 0f;
                 }
             }
-            Object.Destroy(this, 5f);                   
         }
     }
 }
