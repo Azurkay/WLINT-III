d039ced0547edd8135c9980a89bbb49ed41c88ed
Azurkay <baptiste.lassalle38@gmail.com>
2026-09-26 22:28:29 +0200

    BreakForwardGears now slow down the car correctly
    Ce fdp marche enfin fuke le lateupdate

--- WhiteLineInterception III/Assets/Scripts/BreakForwardGears.cs
+++ WhiteLineInterception III/Assets/Scripts/BreakForwardGears.cs
@@ -7,10 +7,13 @@ public class BreakForwardGears : MonoBehaviour
 {
 
     [SerializeField] private CamaroDrive _camaroRef;
+    [SerializeField] private float _speedToSlowDown = 5;
     [SerializeField] private TextMeshProUGUI _explanationGameObject;
     [SerializeField] private String _explanationText = "Oh the forward gears juste broked";
     [SerializeField] private float _timeToDesableExplanation;
     [SerializeField] private AudioSource _audioSource;
+
+    private bool _haveToSlowDown = false;
 
     void Start()
     {
@@ -21,8 +24,10 @@ public class BreakForwardGears : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         _audioSource.Play();
-        _camaroRef.SlowDownCar(300);
+        _haveToSlowDown = true;
+        Debug.Log(_haveToSlowDown);
         Destroy(_explanationGameObject, _timeToDesableExplanation);
+        Destroy(gameObject, _timeToDesableExplanation + 2f);
         _explanationGameObject.gameObject.SetActive(true);
         for (int i = 0; i < _camaroRef.GearsRatios.Length; i++)
         {
@@ -31,5 +36,19 @@ public class BreakForwardGears : MonoBehaviour
                 _camaroRef.GearsRatios[i] = 0f;
             }
          }
+    }
+
+    private void LateUpdate()
+    {
+        if (_haveToSlowDown == true)
+        {
+            _camaroRef.SlowDownCar(6000);
+            Debug.Log("SlowDown");
+            if (_camaroRef.RB.linearVelocity.magnitude * 3.6f < _speedToSlowDown)
+            {
+                _haveToSlowDown = false;
+                Debug.Log("Stop to SlowDown");
+            }
+        }
     }
 }
